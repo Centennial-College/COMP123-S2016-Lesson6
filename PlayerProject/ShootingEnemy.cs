0693d971c62f299aa12f657406fef4abda1528a8
kb-ma <kbma.kevin@gmail.com>
2016-06-18 20:40:59 -0400

    added class diagram for the player project

---
+++ PlayerProject/ShootingEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlayerProject
+{
+    public class ShootingEnemy : Enemy, IShootable, ITouchable
+    {
+        public ShootingEnemy(string name) : base(name)
+        {
+        }
+
+        public void Fight()
+        {
+            Console.WriteLine("{0} is fighting.", this.Name);
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("{0} is shooting.", this.Name);
+        }
+    }
+}
