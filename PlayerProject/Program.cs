66a705d60507ced9cb98a678a7f3ca4b831ef74e
Kevin Ma <kbma.kevin@gmail.com>
2016-06-18 21:28:54 -0400

    updated abstract GameObject class and created Vector2 class

--- PlayerProject/Program.cs
+++ PlayerProject/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+ * Author: Kevin Ma
+ * Date: June 18, 2016
+ * StudentID: 300867968
+ * Description: This Program tests the Player and Enemy Classes
+ * Version: 0.01 - Initial Test Version - testing basic classes and methods
+ */
+
 namespace PlayerProject
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Player greenPlayer = new Player("green");
+            greenPlayer.MoveForward();
+
+            TouchingEnemy redEnemy = new TouchingEnemy("red");
+            redEnemy.MoveForward();
+
+            WaitForAnyKey();
+        }
+
+        /**
+         * <summary>
+         * Utility method to wait for a console key press from the user
+         * </summary>
+         * 
+         * @method WaitForAnyKey
+         * @returns {void}
+         */
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
